13a9dbbcb86c53e63e2b8daa230f14ae64701ac7
Arthur Wesley <50496947+ArthurTGW@users.noreply.github.com>
2022-11-05 16:08:16 -0400

    add keras stuff

--- main.py
+++ main.py
@@ -4,10 +4,26 @@ Author: Arthur Wesley & Camden Ross
 
 """
 
+import sente
+# from sente import sgf
+from tensorflow import keras
+from tensorflow.keras import layers
+
 
 def main():
 
-    print("hello world")
+    game = sente.Game()
+
+    inputs = keras.Input(shape=(784,))
+
+    dense = layers.Dense(64, activation="relu")
+    x = dense(inputs)
+
+    x = layers.Dense(64, activation="relu")(x)
+    outputs = layers.Dense(10)(x)
+
+    model = keras.Model(inputs=inputs, outputs=outputs, name="mnist_model")
+    model.summary()
 
 
 if __name__ == "__main__":
